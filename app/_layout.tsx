aabda74fd7e1a2bad26d7ca8496e2e64403f41f0
Alexandru-Alex <alexstefan_ct@yahoo.com>
2026-04-18 00:53:48 +0300

    fix: hide default header on goals screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout() {
             <Stack.Screen name="edit-entry" options={{ headerShown: false }} />
             <Stack.Screen name="create-categories" options={{ headerShown: false }} />
             <Stack.Screen name="budgets" options={{ headerShown: false }} />
+            <Stack.Screen name="goals" options={{ headerShown: false }} />
           </Stack>
           <StatusBar style="auto" />
         </ThemeProvider>
