a7262a763c88f7c4da8f99bda699cbb4ef2971d5
Alexandru-Alex <alexstefan_ct@yahoo.com>
2026-05-09 14:37:07 +0300

    hidden link

--- lib/flower-images.ts
+++ lib/flower-images.ts
@@ -1,5 +1,8 @@
 import { ImageSourcePropType } from "react-native";
 
+const CLOUDINARY_BASE = "https://res.cloudinary.com/dpzb0itvs/";
+
 export function flowerImage(imageUrl: string): ImageSourcePropType {
-  return { uri: imageUrl };
+  const uri = imageUrl.startsWith("http") ? imageUrl : `${CLOUDINARY_BASE}${imageUrl}`;
+  return { uri };
 }
